af44908d9d780ae6045c2beb0e65672a68b54312
dasPhillipBrau <leggyface@gmail.com>
2018-11-12 14:40:29 -0600

    setup firebase data fetch

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -1,31 +1,30 @@
-//Expenses reducer
+// Expenses Reducer
 
-const expensesReducerDefaultState = []
+const expensesReducerDefaultState = [];
 
 export default (state = expensesReducerDefaultState, action) => {
-    switch (action.type) {
-        case 'ADD_EXPENSE':
-            return [
-                ...state,
-                action.expense
-            ];
-        case 'REMOVE_EXPENSE':
-            return state.filter(({ id }) => {
-                return id !== action.id;
-            })
-        case 'EDIT_EXPENSE':
-            return state.map((expense) => {
-                if (expense.id === action.id) {
-                    return {
-                        ...expense,
-                        ...action.updates
-                    };
-                } else {
-                    return expense;
-                };
-            })        
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [
+        ...state,
+        action.expense
+      ];
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          };
+        } else {
+          return expense;
+        };
+      });
+    case 'SET_EXPENSES':
+      return action.expenses;
+    default:
+      return state;
+  }
 };
-
