e6b1dcb299dabd3fb7428903dbc6fbf163e9a880
dasPhillipBrau <leggyface@gmail.com>
2018-11-12 23:26:08 -0600

    add expensify styles

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -36,7 +36,7 @@ export const removeExpense = ({ id } = {}) => ({
 export const startRemoveExpense = ({ id } = {}) => {
   return (dispatch, getState) => {
     const uid = getState().auth.uid;
-    return database.ref(`users/${uid}expenses/${id}`).remove().then(() => {
+    return database.ref(`users/${uid}/expenses/${id}`).remove().then(() => {
       dispatch(removeExpense({ id }));
     });
   };
